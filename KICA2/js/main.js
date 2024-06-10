3606af0493602db709fc2f8d83e6569ee1929ede
hioozzz <dldudwl2176@naver.com>
2024-06-10 09:37:47 +0900

    Add files via upload

---
+++ KICA2/js/main.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+
+  /* scroll event section */
+  container.addListener((e) => {
+    
+		let scrollTop = container.scrollTop;
+		$('.posNum').html(scrollTop);
+
+
+  });
+
+
+
+
+
+
+
+      
+
+});
+
+
